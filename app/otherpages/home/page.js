4137a0af0652803f33bf7accbc4f17b01931006e
Nayan <nayants@outlook.com>
2024-04-02 14:48:49 +0600

    changes to logged in page

--- app/otherpages/home/page.js
+++ app/otherpages/home/page.js
@@ -2,14 +2,14 @@
 export default function Navbar () {
 
     return (     
-      <div className="flex md:flex-row flex-col">
+        <div className="flex md:flex-row flex-col">
           <div className="flex md:flex-col flex-row justify-between md: items-center  w-screen h-16 md:w-16 md:h-screen md:py-8 md:space-y-8 bg-white dark:bg-gray-900 dark:border-gray-700">
             <a href="#" className="p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100">
               <img src="/Design3.png" className="w-8 h-8" alt="Chharo Logo" />
             </a>
             <div className="flex justify-between md:flex-col md:py-8 md:space-y-8 space-x-4 md:space-x-0">
               <a href="#" className="p-1.5 text-blue-500 transition-colors duration-200 bg-blue-100 rounded-lg dark:text-blue-400 dark:bg-gray-800">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                 </svg>
               </a>
@@ -39,7 +39,7 @@ export default function Navbar () {
             hello
           </div>
         </div>
-          <div>
+          <div className="w-full md:pr-4">
           <main role="main">
                 <div className ="flex w-min-fit">
                     <section className="w-full border border-y-0 border-gray-800 py-8 ">
@@ -53,10 +53,10 @@ export default function Navbar () {
                           <img className="inline-block h-10 w-10 rounded-full" src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt=""/>
                         </div>
                         <div className="flex-1 px-2 pt-2 mt-2">
-                          <textarea className=" bg-transparent text-gray-400 font-medium text-lg w-full" rows="2" cols="50" placeholder="What's happening?"></textarea>
+                          <textarea className="resize-none bg-transparent text-gray-400 font-medium text-lg w-full" rows="2" cols="50" placeholder="What's happening?"></textarea>
                         </div>
                       </div>
-    {/* <!--middle creat tweet below icons--> */}
+    {/* <!--middle create tweet below icons--> */}
     <div className="flex">
         <div className="w-10"></div>
 
@@ -65,7 +65,7 @@ export default function Navbar () {
             <div className="flex items-center">
                 <div className="flex-1 text-center px-1 py-1 m-2">
                     <a href="#" className="mt-1 group flex items-center text-blue-400 px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-gray-800 hover:text-blue-300">
-                        <svg className="text-center h-7 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24">
                             <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z">
                             </path>
                         </svg>
@@ -74,7 +74,7 @@ export default function Navbar () {
 
                 <div className="flex-1 text-center py-2 m-2">
                     <a href="#" className="mt-1 group flex items-center text-blue-400 px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-gray-800 hover:text-blue-300">
-                        <svg className="text-center h-7 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24">
                             <path d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z">
                             </path>
                             <path d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
@@ -84,7 +84,7 @@ export default function Navbar () {
 
                 <div className="flex-1 text-center py-2 m-2">
                     <a href="#" className="mt-1 group flex items-center text-blue-400 px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-gray-800 hover:text-blue-300">
-                        <svg className="text-center h-7 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24">
                             <path d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z">
                             </path>
                         </svg>
@@ -93,7 +93,7 @@ export default function Navbar () {
 
                 <div className="flex-1 text-center py-2 m-2">
                     <a href="#" className="mt-1 group flex items-center text-blue-400 px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-gray-800 hover:text-blue-300">
-                        <svg className="text-center h-7 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg className="text-center h-7 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24">
                             <path d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
                             </path>
                         </svg>
@@ -103,7 +103,7 @@ export default function Navbar () {
         </div>
 
         <div className="flex-1">
-            <button className="bg-blue-400 hover:bg-blue-500 mt-5 text-white font-bold py-2 px-8 rounded-full mr-8 float-right">
+            <button className=" bg-blue-400 hover:bg-blue-500 mt-5 text-white font-bold py-2 px-8 rounded-full mr-8 float-right">
                 Post
             </button>
         </div>
