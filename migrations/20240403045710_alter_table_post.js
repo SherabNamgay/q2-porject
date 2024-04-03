e26705c9f5874c7378426cece61277af0fc70f3f
Nayan <nayants@outlook.com>
2024-04-03 11:10:30 +0600

    add posts

---
+++ migrations/20240403045710_alter_table_post.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('posts', (table) => {
+      table.string('post', 1024).notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('posts', (table) => {
+      table.dropColumn('post');
+  })
+  
+};
