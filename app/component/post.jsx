37b104e17dac1580c24f2477d0ce156af5b958ce
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-04-09 13:15:27 +0600

    added comments route

--- app/component/post.jsx
+++ app/component/post.jsx
@@ -8,6 +8,7 @@ export default function Post() {
     const [post, setPost] = useState('')
     const [feeds, setFeeds] = useState([])
     const {user}= useContext(UserContext)
+    const [comment, setComment] = useState({})
 try{
     let userID= user.id
 
@@ -49,7 +50,34 @@ try{
             })
         })
         getPosts()
-       
+    }
+    async function commentPost(postID) {
+        // try{
+
+            const req = await fetch(`/api/users/${userID}/post/comment`,{
+                method: "POST",
+                body:JSON.stringify({
+                    postID : postID,
+                    comment: comment[postID],
+                })
+            })
+            handleComment(postID, "")
+            getPosts()
+            // if(req.ok){
+                // const updatedPosts = await getPosts();
+                // setFeeds(updatedPosts)
+        //     }else{
+        //         throw new Error("Something went wrong")
+        //     }
+        // }catch(error){
+        //     console.log(error)
+        // }
+    }
+    const handleComment=(postID, comment)=>{
+        setComment(prevState =>({
+            ...prevState,
+            [postID]: comment
+        }))
     }
 
     useEffect(() => {
@@ -159,7 +187,6 @@ try{
                         <div className='flex flex-rows space-x-5'>
                             {/* like button */}
                             <button
-                                key={post.id}
                                 className={"flex flex-rows " + (post.user_liked ? " text-blue-500" : "")}
                                 onClick={() => likePost(post.id)}
                             >
@@ -170,10 +197,31 @@ try{
                                 <p className='text-gray-300 pl-1'>{post.likes}</p>
                             </button>
                             {/* comment button */}
-                            <button className=''>
+                            <button 
+                                className=''
+                            >
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
                                 </svg>
+                            </button>
+                        </div>
+                        <div className='hidden md:block'>  {/* for displaying comment */}
+                            commment from another
+                        </div>
+                        <button className='text-gray-400 pt-1 text-sm'>View all {post.comments} comments</button> {/*TODO*/}
+                        <div className="flex text-sm">
+                            <textarea
+                                value={comment[post.id ||'']}
+                                rows="1"
+                                className="flex w-full bg-transparent h-8 placeholder-oppacity-50 py-1 outline-none "
+                                placeholder="Add a comment"
+                                onChange={(e)=>handleComment(post.id, e.target.value)} //TODO: send comment to DB(e.target.value)}
+                            />
+                            <button 
+                                className={`text-blue-500 ${!comment[post.id] && "hidden"}`}
+                                onClick={()=>{commentPost(post.id)}}
+                            >
+                                Post
                             </button>
                         </div>
                     </div>
