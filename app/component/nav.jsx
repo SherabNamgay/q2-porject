173c87c78bce211496497caf6970d7e4daccf1ab
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-04-11 16:36:56 +0600

    change appstate of profile

--- app/component/nav.jsx
+++ app/component/nav.jsx
@@ -41,7 +41,7 @@ export default function Navbar({
         </button>
 
         <button 
-            onClick={() => router.push('./profile')} 
+            onClick={() => setAppState(PROFILE)} 
             className={`p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100 ${appState === PROFILE?'dark:text-blue-500 bg-gray-800':'dark:text-gray-400'}`}
           >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
